34e93c59992bff1758ce47324285cf3c76276d30
Philippe Bruhat (BooK) <book@cpan.org>
2010-11-04 10:32:58 +0100

    t/lib/TestFunctions.pm: support git init back to git 1.5.0

--- t/lib/TestFunctions.pm
+++ t/lib/TestFunctions.pm
@@ -2,11 +2,15 @@ use strict;
 use warnings;
 
 use File::Temp 'tempdir';
+use Cwd 'cwd';
 use Git::Repository;
 
 sub create_empty_repo {
-    my $dir  = tempdir( CLEANUP => 1 ); 
-    my $repo = Git::Repository->create( init => $dir );
+    my $dir = tempdir( CLEANUP => 1 );
+    my $orig = cwd();
+    chdir $dir or die "Can't chdir back to $dir: $!";
+    my $repo = Git::Repository->create('init');
+    chdir $orig or die "Can't chdir back to $orig: $!";
     return $repo;
 }
 
