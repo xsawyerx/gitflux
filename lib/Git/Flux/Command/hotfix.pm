5f720afba6568254fabd581a04298cc938e45960
franck cuny <franck@lumberjaph.net>
2011-01-03 10:37:43 +0100

    more tests

--- lib/Git/Flux/Command/hotfix.pm
+++ lib/Git/Flux/Command/hotfix.pm
@@ -152,7 +152,7 @@ sub hotfix_finish {
     }
 
     if ( !defined $args->{n} ) {
-        if ( $self->git_tag_exists($tag) ) {
+        if ( !$self->git_tag_exists($tag) ) {
             # TODO sign
             my $cmd = $repo->command( 'tag' => $tag );
             $cmd->close;
