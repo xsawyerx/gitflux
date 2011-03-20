67784760d41fad54f6ebe9546273df54abf0f4ad
franck cuny <franck@lumberjaph.net>
2011-03-20 11:13:10 +0100

    new role for Term::Readline
    Signed-off-by: franck cuny <franck@lumberjaph.net>

---
+++ lib/Git/Flux/Term.pm
@@ -0,0 +1,29 @@
+package Git::Flux::Term;
+
+use Mouse::Role;
+use Term::Readline;
+
+with term => (
+    is      => 'rw',
+    isa     => 'Term::Readline',
+    lazy    => 1,
+    default => sub {
+        my $term = Term::Readline->new('Gitflux');
+        $term->ornaments(0);
+    },
+);
+
+sub is_interactive {
+    return -t STDIN && -t STDOUT;
+}
+
+sub answer {
+    my ( $self, $prompt, $default_suggestion ) = @_;
+
+    my $answer =
+        $self->is_interactive
+      ? $self->term->readline($prompt)
+      : $default_suggestion;
+}
+
+1;
