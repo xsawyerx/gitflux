b4bc0087a66f7f597e82a27d67d9e9973e8e5f1e
franck cuny <franck@lumberjaph.net>
2011-03-20 10:30:18 +0100

    import release

---
+++ lib/Git/Flux/Command/release.pm
@@ -0,0 +1,46 @@
+package Git::Flux::Command::release;
+
+use strict;
+use warnings;
+
+use Git::Flux::Response;
+
+use mixin::with 'Git::Flux';
+
+sub release {
+    my $self = shift;
+    my $cmd  = shift || 'list';
+
+    my $method = "release_$cmd";
+    $self->gitflux_load_settings();
+
+    $self->$method(@_);
+}
+
+sub release_list {
+    my $self = shift;
+
+    my $repo = $self->{repo};
+    my $prefix = $self->feature_prefix();
+
+    my @releases_branches = grep { /^$prefix/ } $self->git_local_branches();
+
+    if ( !scalar @features_branches ) {
+        my $error = qq{
+No release branches exist.
+
+You can start a new release branch:
+
+    gitflux release start <name> [<base>]
+
+};
+        return Git::Flux::Response->new(
+            status => 0,
+            error  => $error
+        );
+    }
+
+    
+}
+
+1;
